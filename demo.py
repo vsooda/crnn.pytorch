a7666661f8087a8349c05e7ec8eaf6e8273b0f7f
vsooda <liushouda@qq.com>
2017-11-24 21:41:35 +0800

    cpu demo

--- demo.py
+++ demo.py
@@ -3,28 +3,34 @@ from torch.autograd import Variable
 import utils
 import dataset
 from PIL import Image
+import keys
 
 import models.crnn as crnn
 
 
-#model_path = './data/crnn.pth'
-model_path = 'expr/netCRNN_499_30.pth'
+model_path = './data/crnn.pth'
 img_path = './data/demo.png'
-alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
+alphabet = keys.alphabet
+nclass = len(alphabet) + 1
+print nclass
 
-model = crnn.CRNN(32, 1, 37, 256)
-if torch.cuda.is_available():
-    model = model.cuda()
+model = crnn.CRNN(32, 1, nclass, 256)
 print('loading pretrained model from %s' % model_path)
-model.load_state_dict(torch.load(model_path))
+state_dict = torch.load(model_path, map_location=lambda storage, loc: storage)
+
+from collections import OrderedDict
+new_state_dict = OrderedDict()
+for k, v in state_dict.items():
+    name = k[7:] # remove `module.`
+    new_state_dict[name] = v
+# load params
+model.load_state_dict(new_state_dict)
 
 converter = utils.strLabelConverter(alphabet)
 
 transformer = dataset.resizeNormalize((100, 32))
 image = Image.open(img_path).convert('L')
 image = transformer(image)
-if torch.cuda.is_available():
-    image = image.cuda()
 image = image.view(1, *image.size())
 image = Variable(image)
 
